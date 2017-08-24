605b4b1d0771411b73f465ade4910539a7305a00
stackia <jsq2627@gmail.com>
2017-08-24 16:57:33 +0800

    feat: 初次提交

---
+++ RemoteRightClickClient/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Application = System.Windows.Application;
+
+namespace RemoteRightClickClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private IKeyboardMouseEvents GlobalHook { get; } = Hook.GlobalEvents();
+        private ClientWebSocket ClientWebSocket { get; } = new ClientWebSocket();
+        private CancellationTokenSource ClientWebSocketCts { get; } = new CancellationTokenSource();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                await ClientWebSocket.ConnectAsync(new Uri("ws://localhost:32768/ws"), ClientWebSocketCts.Token);
+                while (!ClientWebSocketCts.IsCancellationRequested)
+                {
+                    var response = await ReceiveStringAsync(ClientWebSocket, ClientWebSocketCts.Token);
+                    if (string.IsNullOrEmpty(response))
+                    {
+                        if (ClientWebSocket.State != WebSocketState.Open)
+                        {
+                            break;
+                        }
+
+                        continue;
+                    }
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (response == "RightClick" && (!ModeToggleButton.IsChecked ?? false))
+                        {
+                            MouseSimulator.ClickLeftMouseButton();
+                        }
+                    });
+                }
+            }, ClientWebSocketCts.Token);
+        }
+
+        private void ModeToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            GlobalHook.MouseDown += GlobalHookMouseDown;
+        }
+        
+        private void ModeToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            GlobalHook.MouseDown -= GlobalHookMouseDown;
+        }
+
+        private void GlobalHookMouseDown(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (mouseEventArgs.Button != MouseButtons.Right) return;
+            ClientWebSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("RightClick")),
+                WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ClientWebSocketCts.Cancel();
+        }
+
+        private static async Task<string> ReceiveStringAsync(WebSocket socket, CancellationToken ct = default(CancellationToken))
+        {
+            var buffer = new ArraySegment<byte>(new byte[8192]);
+            using (var ms = new MemoryStream())
+            {
+                WebSocketReceiveResult result;
+                do
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    if (socket.CloseStatus.HasValue) return null;
+                    result = await socket.ReceiveAsync(buffer, ct);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                }
+                while (!result.EndOfMessage);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                if (result.MessageType != WebSocketMessageType.Text)
+                {
+                    return null;
+                }
+
+                // Encoding UTF8: https://tools.ietf.org/html/rfc6455#section-5.6
+                using (var reader = new StreamReader(ms, Encoding.UTF8))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+        }
+    }
+}
---
+++ RemoteRightClickServer/Startup.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RemoteRightClickServer
+{
+    public class Startup
+    {
+        private ConcurrentDictionary<string, WebSocket> ClientSockets { get; } = new ConcurrentDictionary<string, WebSocket>();
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var ct = context.RequestAborted;
+                        var currentSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        var socketId = Guid.NewGuid().ToString();
+
+                        ClientSockets.TryAdd(socketId, currentSocket);
+
+                        while (true)
+                        {
+                            if (ct.IsCancellationRequested)
+                            {
+                                break;
+                            }
+
+                            try
+                            {
+                                var response = await ReceiveStringAsync(currentSocket, ct);
+                                if (string.IsNullOrEmpty(response))
+                                {
+                                    if (currentSocket.State != WebSocketState.Open)
+                                    {
+                                        break;
+                                    }
+
+                                    continue;
+                                }
+
+                                foreach (var socket in ClientSockets)
+                                {
+                                    if (socket.Value.State != WebSocketState.Open)
+                                    {
+                                        continue;
+                                    }
+
+                                    await SendStringAsync(socket.Value, response, ct);
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                // ignore
+                            }
+                        }
+
+                        ClientSockets.TryRemove(socketId, out _);
+
+                        if (currentSocket.State == WebSocketState.Open)
+                        {
+                            await currentSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", ct);
+                        }
+                        currentSocket.Dispose();
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+        }
+
+        private static Task SendStringAsync(WebSocket socket, string data, CancellationToken ct = default(CancellationToken))
+        {
+            var buffer = Encoding.UTF8.GetBytes(data);
+            var segment = new ArraySegment<byte>(buffer);
+            return socket.SendAsync(segment, WebSocketMessageType.Text, true, ct);
+        }
+
+        private static async Task<string> ReceiveStringAsync(WebSocket socket, CancellationToken ct = default(CancellationToken))
+        {
+            var buffer = new ArraySegment<byte>(new byte[8192]);
+            using (var ms = new MemoryStream())
+            {
+                WebSocketReceiveResult result;
+                do
+                {
+                    ct.ThrowIfCancellationRequested();
+                    
+                    result = await socket.ReceiveAsync(buffer, ct);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                }
+                while (!result.EndOfMessage);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                if (result.MessageType != WebSocketMessageType.Text)
+                {
+                    return null;
+                }
+
+                // Encoding UTF8: https://tools.ietf.org/html/rfc6455#section-5.6
+                using (var reader = new StreamReader(ms, Encoding.UTF8))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+        }
+    }
+}
